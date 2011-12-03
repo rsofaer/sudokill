dad6c7c50da357147f3484305cdad3a8aa8f2341
Akash Mehra <mehra.akash@gmail.com>
2011-12-02 21:34:09 -0500

    sudodill core Impl functions

---
+++ sudokill_core.cpp
@@ -0,0 +1,23 @@
+#include "sudokill_core.h"
+namespace hps
+{
+namespace sudokill
+{
+
+bool Board::isValidRow(Point<PointType> p, int value)
+{
+  
+}
+
+bool Board::isValidColumn(Point<PointType> p, int value)
+{
+  
+}
+
+bool Board::isValidGrid(Point<PointType> p, int value)
+{
+  
+}
+
+}
+}
