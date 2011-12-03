42505843b93b9e13e061a9905f4999f8bca7ff0a
Akash Mehra <mehra.akash@gmail.com>
2011-12-02 19:44:53 -0500

    Basic structures for sudokill, TODO: add def for ValidMove function and tests for sudokill_core.h

---
+++ sudokill_core.h
@@ -0,0 +1,122 @@
+#ifndef _SUDOKILL_SUDOKILL_CORE_H
+#define _SUDOKILL_SUDOKILL_CORE_H
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+namespace hps 
+{
+
+namespace sudokill
+{
+
+template<typename PointType>
+struct Point
+{
+  Point(PointType x_,
+        PointType y_)
+  : x(x_),
+    y(y_)
+  {}
+  
+  PointType x;
+  PointType y;
+  
+  Point operator+(const Point& rhs) const
+  {
+    return Point(x+rhs.x, y+rhs.y);
+  }
+  
+  bool operator==(const Point& rhs) const
+  {
+    return (x == rhs.x && y == rhs.y);
+  }
+};
+
+template <typename PointType>
+struct Cell
+{
+  Cell(Point<PointType> location_,
+       int value_)
+  : location(location_),
+    value(value_)
+  {}
+  Point<PointType> location;
+  int value;
+};
+  
+template<int MaxX_, int MaxY_, typename PointType>
+struct GenericBoard
+{
+  // max x value.
+  enum {MaxX = MaxX_,};
+  // max y value.
+  enum {MaxY = MaxY_,};
+  
+  typedef std::vector<Cell<PointType> > Positions;
+  Positions positions;
+  
+  /*
+   <summary> This function puts the value at a point</summary>
+   <parameters> A generic point Point and the value to be played.</parameters>
+   */
+  inline void PlayMove(Point<PointType> p, int value)
+  {
+    //assert(p.x >= 0 && p.x <= MaxX);
+    //assert(p.y >= 0 && p.y <= MaxY);
+    
+    typename Positions::iterator pos = std::find(positions.begin(),positions.end(),p);
+    
+    //assert if position occupied.
+    assert(pos == positions.end());
+    
+    //position is set by creating an object of type Cell.
+    Cell<PointType> c(p,value);
+    positions.push_back(c);
+  }
+  
+  /*
+   <summary> This function is used to Undo the last move.</summary>
+   */
+  inline void Undo()
+  {
+    assert(positions.size() > 0);
+    // the last value played is put at the back.
+    positions.pop_back();
+  }
+  
+  /*
+   <summary> This function returns the value at a point</summary>
+   <parameters> A generic point Point</parameters>
+   <returns> Value at point p or -1 if the location is empty.</returns>
+   */
+  inline int ValueAt(Point<PointType> p)
+  {
+    assert(p.x >= 0 && p.x <= MaxX);
+    assert(p.y >= 0 && p.y <= MaxY);
+    int retVal = -1;
+    
+    typename Positions::iterator pos = std::find(positions.begin(),positions.end(),p);
+    
+    if(positions != positions.end())
+    {
+      retVal = (*pos).value;
+    }
+      
+    return retVal;
+  }
+  
+  inline bool ValidMove(Point<PointType> p, int value)
+  {
+    //TODO:
+    // check if row is valid
+    // check if column is valid.
+    // check if grid is valid.
+  }
+};
+}
+}
+
+
+#endif // _SUDOKILL_SUDOKILL_CORE_H
