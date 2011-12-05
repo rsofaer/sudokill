3edebd504067faf06e307a51fc9e1d0793bc206d
Raphael Sofaer <raphael@joindiaspora.com>
2011-12-05 00:03:21 -0500

    Set up test files.

---
+++ sudokill_core_gtest.h
@@ -0,0 +1,9 @@
+#ifndef _HPS_SUDOKILL_CORE_GTEST_H_
+#define _HPS_SUDOKILL_CORE_GTEST_H_
+
+#include "sudokill_core.h"
+#include "gtest/gtest.h"
+
+
+
+#endif //_HPS_SUDOKILL_CORE_GTEST_H_
---
+++ sudokill_gtest.cpp
@@ -0,0 +1,21 @@
+#include "sudokill_core_gtest.h"
+#include "gtest/gtest.h"
+#ifdef WIN32
+#include <time.h>
+#elif __APPLE__
+#include <time.h>
+#else
+#include <sys/time.h>
+#endif
+
+int main(int argc, char** argv)
+{
+  const unsigned int randSeed = static_cast<unsigned int>(time(NULL));
+  //const unsigned int randSeed = 1319414691;
+  //const unsigned int randSeed = 1319433120;
+  std::cout << "Random seed: " << randSeed << "." << std::endl;
+  srand(randSeed);
+  testing::InitGoogleTest(&argc, argv);
+  testing::FLAGS_gtest_catch_exceptions = false;
+  return RUN_ALL_TESTS();
+}
