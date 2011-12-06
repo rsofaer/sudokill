ad08205d86a87f27abc029f85f75b77b289096c1
Raphael Sofaer <raphael@joindiaspora.com>
2011-12-05 21:47:52 -0500

    Finish inSameRowOrColumnIfPossible, waiting on isValidBox to re-enable the tests

--- sudokill_core.h
+++ sudokill_core.h
@@ -154,9 +154,20 @@ struct GenericBoard
         return true;
       } else
       {
-        // This is wrong.  We must check that there are possible moves
-        // within the row/column.
-        return false;
+        move_list_type validMoves;
+        ValidMoves(validMoves);
+        for(unsigned int i = 0; i < validMoves.size(); i++)
+        {
+          point_type p = validMoves[i].location;
+          if(p.x == lastPlay.x || p.y == lastPlay.y)
+          {
+            // There is a valid move within the row or column which the last
+            // move was in.
+            return false;
+          }
+        }
+        // There are no valid moves within the row or column.
+        return true;
       }
     }
   }
