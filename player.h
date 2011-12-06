63a98ea999655b6f2448263842a49e8c3ba756ae
Brandon Reiss <brandon.l.reiss@gmail.com>
2011-12-06 14:11:19 -0500

    Merging manually from master/player.h. Does not compile.

--- player.h
+++ player.h
@@ -1,11 +1,27 @@
 #ifndef _SUDOKILL_PLAYER_H
 #define _SUDOKILL_PLAYER_H
+#include "rand_bound.h"
 #include "alphabetapruning.h"
 
 namespace hps 
 {
 namespace sudokill
 {
+
+  struct RandomPlayer
+  {
+    void NextMove(const Board& board, Cell* move)
+    {
+      Board::MoveList moves = board.ValidMoves();
+      if(moves.size() == 0) // We have lost!
+      {
+        // Pick any empty cell.
+      }else
+      {
+        move = &(moves[math::RandBound(moves.size())]);
+      }
+    }
+  }
 
 /// <summary> Player using alpha-beta pruning. </summary>
 class AlphaBetaPlayer
