152944a41aa05c87955804071120358905bc667d
Brandon Reiss <brandon.l.reiss@gmail.com>
2011-12-06 13:51:22 -0500

    Implementing alphabeta player. Does not compile since it needs AnyPlyWillDo().

--- player.h
+++ player.h
@@ -1,10 +1,38 @@
 #ifndef _SUDOKILL_PLAYER_H
 #define _SUDOKILL_PLAYER_H
+#include "alphabetapruning.h"
 
 namespace hps 
 {
 namespace sudokill
 {
+
+/// <summary> Player using alpha-beta pruning. </summary>
+class AlphaBetaPlayer
+{
+  /// <summary> Try to estimate the number of reachable cells left. </summary>
+  struct ShrinkPossibleMovesEvaluationFunc
+  {
+    inline int operator()(const Board& board) const
+    {
+      -board.SudokuValidMoves();
+    }
+  };
+public:
+  AlphaBetaPlayer();
+
+  /// <summary> Return the next move for the player. </summary>
+  void NextMove(const Board& board, Cell* move)
+  {
+    AlphaBetaPruning::Run(&params, &board,
+                          ShrinkPossibleMovesEvaluationFunc(),
+                          &move);
+  }
+
+private:
+  AlphaBetaPruning::Params params;
+};
+
 }
 }
 #endif // _SUDOKILL_PLAYER_H
